3261d75f4d797c1dc4e5355bbc7d57650e608d79
Kureykin <dombrauskas06@gmail.com>
2026-03-23 02:01:58 +0100

    Update StartWindow.java

--- src/main/java/game/frontend/StartWindow.java
+++ src/main/java/game/frontend/StartWindow.java
@@ -11,32 +11,45 @@ import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
 public class StartWindow {
 
-    private final JFrame frame = new JFrame("Welcome");
+    private final JFrame frame;
 
     public StartWindow() {
+        frame = new JFrame("Welcome");
         frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
         frame.setBounds(760, 490, 400, 100);
+        frame.setContentPane(panelConfig());
+        frame.setVisible(true);
+        }
 
-        JPanel panel = new JPanel();
-        panel.setLayout(null);
-
-        JLabel text = new JLabel("Welcome to the game!!!");
+    private JLabel textConfig() {
+        JLabel text = new JLabel("Ласкаво просимо до гри!!!");
         text.setHorizontalAlignment(SwingConstants.CENTER);
         text.setSize(200,60);
-        JButton startButton = new JButton("Start!!");
+
+        return text;
+    }
+
+    private JButton buttonConfig() {
+        JButton startButton = new JButton("Старт!!");
         startButton.setSize(80,30);
         startButton.setVerticalAlignment(SwingConstants.CENTER);
         startButton.setLocation(233, 15);
         startButton.addActionListener(new StartButtonAction());
 
-        panel.add(text);
-        panel.add(startButton);
+        return startButton;
+    }
+
+    private JPanel panelConfig() {
+        JPanel panel = new JPanel();
+        panel.setLayout(null);
+
+        panel.add(textConfig());
+        panel.add(buttonConfig());
 
         panel.setVisible(true);
 
-        frame.setContentPane(panel);
-        frame.setVisible(true);
-        }
+        return panel;
+    }
 
     private class StartButtonAction implements ActionListener {
 
